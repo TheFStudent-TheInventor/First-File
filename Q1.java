c8ff52f7a9eae27bfe58f9e9580fdb4703353fd0
TheFStudent-TheInventor <williams275@nycstudents.net>
2025-05-15 09:37:56 -0400

    Update Q1.java

--- Q1.java
+++ Q1.java
@@ -1,1 +1,10 @@
-
+public int walkDogs(int hour){
+        int numDogs = numAvailableDogs(hour);
+        if(max > numDogs){
+            company.updateDogs(hour,numDogs);
+            return numDogs;
+        }
+        company.updateDogs(hour, maxDogs);
+        return maxDogs;
+    }
+public 

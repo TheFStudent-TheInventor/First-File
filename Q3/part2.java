3692257b9e5be57909a08c64ac7fc5af629bf7e7
TheFStudent-TheInventor <williams275@nycstudents.net>
2025-06-04 00:32:16 -0400

    Update part2.java

--- Q3/part2.java
+++ Q3/part2.java
@@ -1,6 +1,6 @@
 public ArrayList<Match> buildMatches(){
         ArrayList<Match> matches = new ArrayList<Match>();
-        if(competitorList.size()%2 == 1){
+        if(competitorList.size()%2 ==0){
                 for(int i = 0; i< competitorList.size();i++){
                         Match current = new Match(competitorList.get(i), competitorList.get(competitorList.size()-i-1));
                         matches.add(current);

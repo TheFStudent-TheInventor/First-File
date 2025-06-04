c37ed4fcc2f3a716b33922195c31404869d7b6fd
TheFStudent-TheInventor <williams275@nycstudents.net>
2025-06-04 00:24:59 -0400

    Update part1.java

--- Q3/part1.java
+++ Q3/part1.java
@@ -1,5 +1,6 @@
 public Round(String [] names){
         for(int i = 0; i < names.length;i++){
                 Competitor current = new Competitor(names[i],i+1);
+                competitorList.add(current);
         }
 }
